52415ba7194063d40d32f939f873f8e28c2b5018
Shadan Asad <55396429+shadan-asad@users.noreply.github.com>
2025-05-25 21:32:36 +0530

    clearing the repo for updated code

--- src/app/features/sql-query/sql-query.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-
-export const SQL_QUERY_ROUTES: Routes = [
-  {
-    path: '',
-    loadComponent: () => import('./components/query-editor/query-editor.component').then(m => m.QueryEditorComponent)
-  }
-]; 
